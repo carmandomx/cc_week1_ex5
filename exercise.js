5f46623716fa02b11a84fe55017601e4351d759c
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-09-20 17:11:44 -0500

    Modified exercise.js

--- exercise.js
+++ exercise.js
@@ -11,7 +11,22 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+
+    let flatArr = []; // declaring an array literal to add the flat elements
+
+        // Loop through all the elements in the given array
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                // When current element is an array, 
+                // Call again flatten() to do the task and then add the result to the flat array
+                flatArr = flatArr.concat(flatten(arr[i]));
+            } else {
+                 // otherwise, when current element ISN'T an array, just add the element directly 
+                flatArr.push(arr[i]); 
+        }
+    }
+    return flatArr;
+
     /* Only make changes below this comment */
 }
 
