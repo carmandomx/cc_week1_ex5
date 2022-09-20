67c1a4fbbb76101ad4a2a502592d61067980e25e
Navidarito Chen <Navidarito.Chen@itksquare.edu.mx>
2022-09-20 00:59:09 -0400

    uploading exercise 5

--- exercise.js
+++ exercise.js
@@ -11,7 +11,22 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    //store the result of the reduce in flattenArr
+    let flattenArr = arr.reduce((a,item)=>{
+        if(Array.isArray(item)){
+            //if the element is an array, use again the flatten function to flatten the array
+            a = a.concat(flatten(item));
+        }
+        else{
+            //not array so add to a
+            a.push(item);
+        }
+        return a;
+    },[]);
+    //return the flatten array
+    return flattenArr;
+
+
     /* Only make changes below this comment */
 }
 
