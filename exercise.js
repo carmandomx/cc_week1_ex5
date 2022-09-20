ab0cbb5e57fd0224d77a6fb172f26b0a0bef32f3
Fredy Guzman <fredy.guzman@itksquare.edu.mx>
2022-09-20 14:56:10 -0500

    Answered

--- exercise.js
+++ exercise.js
@@ -11,7 +11,12 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    if(arr.length===0){
+        return [];
+    }else{
+        let flattenArray = arr.flat(3);
+        return flattenArray;
+    }
     /* Only make changes below this comment */
 }
 
