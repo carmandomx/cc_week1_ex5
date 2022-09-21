bd1e6aaa001708f671e877b42f4a8c9631f7ea62
Axcel Espinal <axcel.espinal@itksquare.edu.mx>
2022-09-21 11:34:16 -0400

    Initial commit

--- exercise.js
+++ exercise.js
@@ -11,12 +11,17 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    let res = [];
+    res = [].concat(...arr);
+    for(i=0; i<res.length; i++) {
+        if (Array.isArray(res[i])) {
+            res = [].concat(...res);
+            i = 0;
+        } 
+    }
     /* Only make changes below this comment */
+    return res;
 }
-
-
-
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
