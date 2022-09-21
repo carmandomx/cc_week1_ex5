b05a368ce26427a4701e0f16e435184fc6554526
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-21 11:29:03 -0500

    Repo fc-flatten v1

--- exercise.js
+++ exercise.js
@@ -11,27 +11,29 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
   /* Only make changes below this comment */
-  /* return arr.reduce(
-    (acc, val) =>
-      Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val),
-    []
-  ); */
   let newArrFlatted = [];
-  for (const indexValue of arr) {
-    if (Array.isArray(indexValue)) {
-      newArrFlatted = newArrFlatted.concat(flatten(indexValue));
-    } else {
-      newArrFlatted.push(indexValue);
+  if (!arr) {
+    //Validation if "arr" exist (true); in case that result false we send a warning.
+    return console.warn("Please input any value in a array");
+  } else if (!Array.isArray(arr)) {
+    //validation in case that "arr" not be a Array, we send another alert.
+    return console.warn("First write a array");
+  } else {
+    //if "arr" exist (true) and is a Array, we do a cicle for-of for iterate our array values.
+    for (const indexValue of arr) {
+      /* case true for array. Per value of index (indexValue) we evaluate if is a Array (sub-array in "arr") and if that it's True then our new array (newArrFlatted) the result of evaluate itself (recursive function) for next that merge it (concat) to "newArrFlatted" */
+      if (Array.isArray(indexValue)) {
+        newArrFlatted = newArrFlatted.concat(flatten(indexValue));
+      } else {
+        //opposite case, when all values of arr ("indexValue") not be sub-arrays, then we can push them in the new array.
+        newArrFlatted.push(indexValue);
+      }
     }
+    //and return it.
+    return newArrFlatted;
   }
-  return newArrFlatted;
   /* Only make changes below this comment */
 };
-
-console.log(flatten([])); //
-console.log(flatten([1, {}, [3, [[4]]]])); //([1, {}, 3, 4]);
-console.log(flatten([1, [], [3, [[4]]]])); //([1, 3, 4]);
-//console.log(!file.match(/\.\s*flat\s*\(/) && !file.match(/\.\s*flatMap\s*\(/)); //true
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.flatten = flatten;
