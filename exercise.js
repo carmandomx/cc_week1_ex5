3e81dd03fbb533f780b4e3d70fff200d55d1d246
Fredy Guzman <fredy.guzman@itksquare.edu.mx>
2022-09-21 15:54:26 -0500

    Resolved

--- exercise.js
+++ exercise.js
@@ -10,12 +10,25 @@
 */
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
+    
     /* Only make changes below this comment */
+    let finalArray=[];
+
+    // If arr is empty
     if(arr.length===0){
-        return [];
+        return finalArray;
     }else{
-        let flattenArray = arr.flat(3);
-        return flattenArray;
+        let arrSize = arr.length;
+        // Iterate in every arr element and pushing it in the final array if it isn't another array.
+        for(let i=0;i<arrSize;i++){
+            // If element is an array, use recursion to flat it
+            if(Array.isArray(arr[i])){
+                finalArray= finalArray.concat(flatten(arr[i]));
+            }else{
+                finalArray.push(arr[i]);
+            }
+        }
+        return finalArray;
     }
     /* Only make changes below this comment */
 }
