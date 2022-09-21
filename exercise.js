ad21168f7afd0c57e32908a7c3bc00bdace94070
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-20 22:51:51 -0500

    update repo-solution Flatten

--- exercise.js
+++ exercise.js
@@ -10,14 +10,28 @@
 */
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
+  /* return arr.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val),
+    []
+  ); */
+  let newArrFlatted = [];
+  for (const indexValue of arr) {
+    if (Array.isArray(indexValue)) {
+      newArrFlatted = newArrFlatted.concat(flatten(indexValue));
+    } else {
+      newArrFlatted.push(indexValue);
+    }
+  }
+  return newArrFlatted;
+  /* Only make changes below this comment */
+};
 
-
-
-
+console.log(flatten([])); //
+console.log(flatten([1, {}, [3, [[4]]]])); //([1, {}, 3, 4]);
+console.log(flatten([1, [], [3, [[4]]]])); //([1, 3, 4]);
+//console.log(!file.match(/\.\s*flat\s*\(/) && !file.match(/\.\s*flatMap\s*\(/)); //true
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.flatten = flatten;
