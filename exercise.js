387a0c90533beff00df4bfa47b71aa642ff9936f
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-09-22 17:06:46 -0500

    exercise solved

--- exercise.js
+++ exercise.js
@@ -12,6 +12,27 @@
 const flatten = (arr) => {
     /* Only make changes below this comment */
     
+    //Using the reduce method
+    let arr2 = arr.reduce((acc, i) => {
+        
+        //This checks if there is an array inside the array
+        if(Array.isArray(i)){
+            
+            //If true then recursive calling the function again and do it again
+            acc = acc.concat(flatten(i));
+            
+        }else{
+            //If there is no array within the array push element
+            acc.push(i)
+        }
+        //Return acc variable after the all the recursions
+        return acc;
+    }, []);
+
+    //Returns the new array without the nested arrays
+    return arr2;
+
+
     /* Only make changes below this comment */
 }
 
