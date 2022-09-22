6a46b73f43d7dc07e63dd841da05b87ce4290c53
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-09-22 17:25:26 -0500

    draft3

--- exercise.js
+++ exercise.js
@@ -11,12 +11,12 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-let result= [];
-arr.forEach((item) => {
-    if(Array.isArray(item)){
-     result = result.concat(flatten(item));
+let result= [];    
+arr.forEach((item) => {  // we make the code below for each element  inside of the array
+        if(Array.isArray(item)){ //then we concatenate each element inside of the result if the result is an array 
+     result = result.concat(flatten(item));// we use a recursive function
     } else {
-    result.push(item);
+    result.push(item);  //as we are not receiving an array perse we just put it into our array result
     }
 },[]);
 return result;
