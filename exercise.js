dedd5846fbf678923114dbed61e4031ccf6b50c4
SergioEspadas-Ksquare <125910030+SergioEspadas-Ksquare@users.noreply.github.com>
2023-03-03 14:48:53 -0600

    Add files via upload

--- exercise.js
+++ exercise.js
@@ -11,12 +11,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    let res = []
+
+    for (let i = 0; i < arr.length; i++) { //analize every element of the array
+        if (Array.isArray(arr[i])) { //check if the object is an array
+            let flat = flatten(arr[i]);
+            res.push(...flat); //put elements in a new single array
+        }
+        else {
+            res.push(arr[i])//put elements in a new single array
+        }
+    }
+    return res;
     /* Only make changes below this comment */
 }
 
 
-
+var arr = [1,2,3,[1,2,3,4, [2,3,4]]]
+console.log(flatten(arr))//[1,2,3,1,2,3,4,2,3,4]
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
