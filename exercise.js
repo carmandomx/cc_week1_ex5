6eda3342de28e6ae358ddb2a5f178af1ac82c54b
Alexis Guevara <alexis.guevara@itksquare.edu.mx>
2023-03-03 08:39:21 -0800

    Ex5 test passed

--- exercise.js
+++ exercise.js
@@ -11,8 +11,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
+
+    let res = []; // New array to contain the result of the original array
+
+    /* forEach to loop through each element of the array  */
+    arr.forEach(element => {
+        /* Here we detect if an element of the array is another array */
+        if(Array.isArray(element)) {
+            /* Add each elemement of the inside array to the final array */
+            res = res.concat(flatten(element));
+        }
+        /* If is not we just push the element to the new array */
+        else {
+            res.push(element);
+        }
+    });
     
     /* Only make changes below this comment */
+    return res; // Return the new array
 }
 
 
