c59f82dee16136b20e88737350f2ac5270ef0a2c
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-03-03 13:46:07 -0400

    Completed exercise

--- exercise.js
+++ exercise.js
@@ -10,14 +10,22 @@
 */
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
+  ///////////// This approach uses reccursion /////////////
 
+  // Use the reduce method to iterate through each element of the array and flatten nested arrays.
+  let flattenedArr = arr.reduce(
+    (acc, val) =>
+      // If the current element is an array:
+      // reccursively call the function "flatten" on the sub-array and concatenate this to the new array.
+      // If not, just add the element to the new array
+      Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val),
+    []
+  );
 
-
-
+  return flattenedArr;
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.flatten = flatten;
