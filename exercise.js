dfbe371492eda4e397741df691dbf0fc909db917
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-03 11:51:48 -0600

    ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -11,7 +11,17 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    let arr2 = [];
+    for(let i=0;i<arr.length;i++){
+        let item = arr[i];
+        if(Array.isArray(item)==true){
+            arr2 = arr2.concat(flatten(item));
+        }else{
+            arr2.push(item)
+        }
+    }
+
+    return arr2;
     /* Only make changes below this comment */
 }
 
