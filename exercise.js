088b6353959a62b6b4847ce38f21f7034555e52f
Rita Torres <taraumara96@gmail.com>
2023-03-03 11:53:48 -0600

    NoUsarFiltter

--- exercise.js
+++ exercise.js
@@ -11,12 +11,19 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    //let nuevo1=arr.flat(infinity);
+    let nuevo1=[]
+    for(var i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+          nuevo1=nuevo1.concat(flatten(arr[i]));  
+        }else {
+         nuevo1.push(arr[i])
+        }
+      
+    }
+     return nuevo1;
     /* Only make changes below this comment */
 }
-
-
-
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
